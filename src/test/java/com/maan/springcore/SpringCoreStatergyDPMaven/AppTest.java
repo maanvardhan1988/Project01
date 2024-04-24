b0188d50042588f0c79face78bebb08f5ab57fc1
maanvardhan1988 <maanvardhan1988@gmail.com>
2024-04-24 22:58:17 +0530

    initial commit

---
+++ src/test/java/com/maan/springcore/SpringCoreStatergyDPMaven/AppTest.java
@@ -0,0 +1,20 @@
+package com.maan.springcore.SpringCoreStatergyDPMaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
